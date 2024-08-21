4dc27633e7c84af1cefd9cf11ac5392644592958
rayotomson <tomas.trebino@gmail.com>
2024-08-20 21:13:44 -0300

    agregado .js

---
+++ app.js
@@ -0,0 +1,15 @@
+let textarea = "inicializada";
+let textoIn = "void";
+
+function mostrarTexto(){
+textoIn = document.getElementById('valorUsuario').value;
+console.log(textoIn);
+
+}
+
+function mostrarVariables(){
+    console.log(textoIn);
+}
+
+console.log(textarea);
+console.log(textoIn);
