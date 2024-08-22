68bc99e8fe2f75e101ca4139a4338c6a34b2d30e
rayotomson <tomas.trebino@gmail.com>
2024-08-21 21:03:42 -0300

    funciones de js de encriptado

--- app.js
+++ app.js
@@ -1,15 +1,55 @@
-let textarea = "inicializada";
-let textoIn = "void";
+let textarea = "";
+let textoIn = "";
 
-function mostrarTexto(){
+function encriptar(){
 textoIn = document.getElementById('valorUsuario').value;
 console.log(textoIn);
+textoIn =encriptarTexto(textoIn);
+document.getElementById("imagen").style.display = "none";
+asignarTextoElemento('p', textoIn);
 
 }
 
-function mostrarVariables(){
+function desencriptar(){
+    textoIn = document.getElementById('valorUsuario').value;
     console.log(textoIn);
+    textoIn =desencriptarTexto(textoIn);
+    document.getElementById("imagen").style.display = "none";
+    asignarTextoElemento('p', textoIn);    
 }
 
-console.log(textarea);
-console.log(textoIn);
+function myFunction() {
+    document.getElementById("imagen").style.display = "none";
+  }
+
+  function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function encriptarTexto(textoIn) {
+    // Reemplaza cada letra según las reglas especificadas
+    let textoEncriptado = textoIn
+        .replace(/e/g, "enter")
+        .replace(/i/g, "imes")
+        .replace(/a/g, "ai")
+        .replace(/o/g, "ober")
+        .replace(/u/g, "ufat");
+    
+    return textoEncriptado;
+}
+
+function desencriptarTexto(textoIn) {
+    // Reemplaza cada letra según las reglas especificadas
+    let textoDesencriptado = textoIn
+    .replace(/enter/g, "e")
+    .replace(/imes/g, "i")
+    .replace(/ai/g, "a")
+    .replace(/ober/g, "o")
+    .replace(/ufat/g, "u");
+    
+    return textoDesencriptado;
+}
+
+
